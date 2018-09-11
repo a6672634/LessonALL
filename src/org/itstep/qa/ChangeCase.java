44984a4ae7442834fa17697c89af8ac321de7354
qa <xbx@jn.dd>
2018-09-11 19:29:13 +0300

    ewt

--- src/org/itstep/qa/ChangeCase.java
+++ src/org/itstep/qa/ChangeCase.java
@@ -8,12 +8,39 @@ public class ChangeCase {
         //Изменить регистр введенного символа, если он был латинской буквой.
         Scanner scanner = new Scanner(System.in);
         char symbol = scanner.next().charAt(0);
-        if(symbol >= 65 && symbol <= 90 || symbol >= 97 && symbol <= 122){
-            if(Character.isLowerCase(symbol))
+        if (symbol >= 65 && symbol <= 90 || symbol >= 97 && symbol <= 122) {
+            if (Character.isLowerCase(symbol))
                 symbol = Character.toUpperCase(symbol);
             else
                 symbol = Character.toLowerCase(symbol);
         }
         System.out.println(symbol);
+
+        System.out.println("--------------------");
+
+        //Решение от преподавателя
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите один символ");
+        String tempValue = scanner1.next();
+        if (tempValue.length() == 1) {
+            char ch = tempValue.charAt(0);
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                if (Character.isLowerCase(ch)) {
+                    System.out.println("Результат = " + Character.toUpperCase(ch));
+                } else {
+                    System.out.println("Результат = " + Character.toLowerCase(ch));
+                }
+            } else {
+                System.out.println("Вы ввели не латинскую букву");
+            }
+        } else {
+            System.out.println("Вы ввели слишком длинное значение");
+
+
+        }
     }
 }
+
+
+
--- src/org/itstep/qa/WordsCounter.java
+++ src/org/itstep/qa/WordsCounter.java
@@ -1,5 +1,7 @@
 package org.itstep.qa;
 
+import java.util.Arrays;
+
 public class WordsCounter {
     public static void main(String[] args) {
         //Дана строка, содержащая пробелы. Найдите, сколько в ней слов (слово – это последовательность символов, отличных от пробела).
@@ -7,5 +9,31 @@ public class WordsCounter {
         String str = "Дана строка, содержащая пробелы.";
         String[] array = str.split(" ");
         System.out.println("String contains " + array.length + " words");
+
+
+        System.out.println("-------------------");
+
+        String testLine = "Мама  мыла раму";
+        String[] result = testLine.split(" ");
+        System.out.println(Arrays.toString(result));
+        int counter = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (!result[i].isEmpty()) {
+                counter++;
+            }
+        }
+        // Это форич так пишут тоже
+        for(String tempValue : result)
+            if(!tempValue.isEmpty()){
+            counter++;
+            }
+        System.out.println("Количество слов равно " + counter);
+
+        System.out.println("---------------------");
+
+        String testLine1 = "Мама  мыла раму";
+        String[] result2 = testLine1.split(" +");
+        System.out.println(Arrays.toString(result2));
+        System.out.println("Количество слов равно " + result2.length);
     }
 }
